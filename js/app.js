36f00d8931633478d5e83cf08608c23086c73323
zawmyolwin11 <tarsalai64@gmail.com>
2024-02-04 09:31:25 -0800

    donea

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let bars = document. querySelector("#bars")
+let navlinks = document. querySelector(".nav-links")
+
+bars.addEventListener("click", function (){
+    // alert("hello")
+    navlinks.clssdist.toggle("show")
+    bars.classList.toggle("fa-times")
+})
